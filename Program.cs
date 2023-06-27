19bff66dbbdd80a7585f53deb050df226c5853e9
LebedevKo <sok-is@mail.ru>
2023-06-27 18:53:39 +0400

    Ввод строк и заполнение исходного массива

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 //Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 
+Console.WriteLine("Введите количество элементов массива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+string[] originalArray = new string[count];
+string[] newArray;
+int resultCount = 0;
+
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine("Введите строку:");
+    originalArray[i] = Console.ReadLine();
+}
